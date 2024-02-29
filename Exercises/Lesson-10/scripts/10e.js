ada9dabdc9d209b88ea8b9daf8d8b7e35efe1cc6
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-29 03:13:39 -0300

    finished the exercises 10e and 10f

---
+++ Exercises/Lesson-10/scripts/10e.js
@@ -0,0 +1,8 @@
+function buttonToggle(selector) {
+  const button = document.querySelector(selector);
+  if (!button.classList.contains('is-toggled')) {
+    button.classList.add('is-toggled');
+  } else {
+    button.classList.remove('is-toggled');
+  }
+}
