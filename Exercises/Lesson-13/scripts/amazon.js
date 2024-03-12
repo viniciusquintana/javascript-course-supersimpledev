ef22df194c08fe4b3c325e0a8d17a0f94a47635b
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-03-12 04:07:47 -0300

    finished the exercise 13m

--- Exercises/Lesson-13/scripts/amazon.js
+++ Exercises/Lesson-13/scripts/amazon.js
@@ -59,6 +59,8 @@ document.querySelector('.js-products-grid')
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
+    let addedMessageTimeoutId;
+
     button.addEventListener('click', () => {
       const {productId} = button.dataset;
 
@@ -95,9 +97,15 @@ document.querySelectorAll('.js-add-to-cart')
 
       addedMessage.classList.add('added-to-cart-visable');
       
-      setTimeout(() => {
+      if (addedMessageTimeoutId) {
+        clearTimeout(addedMessageTimeoutId);
+      }
+
+      const timeoutId = setTimeout(() => {
         addedMessage.classList.remove('added-to-cart-visable');
-      }, 2000)
+      }, 2000);
+      
+      addedMessageTimeoutId = timeoutId;
     });
   });
 
