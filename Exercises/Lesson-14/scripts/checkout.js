e8b50fcb91297a2dfc7a6510f1ef155dc7d7b581
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-03-19 04:49:34 -0300

    finished the exercise 14j

--- Exercises/Lesson-14/scripts/checkout.js
+++ Exercises/Lesson-14/scripts/checkout.js
@@ -43,7 +43,8 @@ cart.forEach((cartItem) => {
               Update
             </span>
             <input class="quantity-input">
-            <span class="save-quantity-link link-primary">Save</span>
+            <span class="save-quantity-link link-primary js-save-link"
+              data-product-id="${matchingProduct.id}">Save</span>
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
@@ -133,6 +134,18 @@ document.querySelectorAll('.js-update-link')
       
       const container = document.querySelector(`.js-cart-item-container-${productId}`
       );
-      container.classList.add('is-editing-quantity')
+      container.classList.add('is-editing-quantity');
+    });
+  });
+
+document.querySelectorAll('.js-save-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.classList.remove('is-editing-quantity');
     });
   });
