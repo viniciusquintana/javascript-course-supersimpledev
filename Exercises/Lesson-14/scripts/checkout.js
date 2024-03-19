e34b36eecf3f16b00cac4a244df5ee5846fd7f3b
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-03-19 04:14:36 -0300

    finished the exercise 14h

--- Exercises/Lesson-14/scripts/checkout.js
+++ Exercises/Lesson-14/scripts/checkout.js
@@ -130,6 +130,9 @@ document.querySelectorAll('.js-update-link')
   .forEach((link) => {
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
-      console.log(productId);
+      
+      const container = document.querySelector(`.js-cart-item-container-${productId}`
+      );
+      container.classList.add('is-editing-quantity')
     });
   });
