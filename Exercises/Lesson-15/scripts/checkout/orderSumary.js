22b79731e3b6740cc051c88d073c35532db31b35
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-03-25 03:08:27 -0300

    finished the exercise 15h

--- Exercises/Lesson-15/scripts/checkout/orderSumary.js
+++ Exercises/Lesson-15/scripts/checkout/orderSumary.js
@@ -122,10 +122,7 @@ export function renderOrderSummary() {
         const productId = link.dataset.productId;
         removeFromCart(productId);
 
-        const container = document.querySelector(
-          `.js-cart-item-container-${productId}`
-        );
-        container.remove();
+        renderOrderSummary();
 
         renderPaymentSummary();
       });
