be17661bfeec4f40385e74e96e24b9a8bb85c855
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-04-18 07:38:32 -0300

    finished the exercise 17c

--- Exercises/Lesson-17/data/car.js
+++ Exercises/Lesson-17/data/car.js
@@ -2,13 +2,33 @@ class Car {
   brand;
   model;
 
+  speed = 0;
+
   constructor(carDetails) {
     this.brand = carDetails.brand;
     this.model = carDetails.model;
   }
 
   displayInfo() {
-    console.log(`${this.brand} ${this.model}`);
+    console.log(
+      `${this.brand} ${this.model}, Speed: ${this.speed}km/h`
+    );
+  }
+
+  go() {
+    this.speed += 5;
+
+    if (this.speed > 200) {
+      this.speed = 200;
+    }
+  }
+
+  brake() {
+    this.speed -= 5;
+
+    if (this.speed < 0) {
+      this.speed = 0;
+    }
   }
 }
 
@@ -25,4 +45,14 @@ console.log(car1);
 console.log(car2);
 
 car1.displayInfo();
+car1.go();
+car1.go();
+car1.go();
+car1.brake();
+car1.displayInfo();
+
+car2.displayInfo();
+car2.go();
+car2.go();
+car2.brake();
 car2.displayInfo();
