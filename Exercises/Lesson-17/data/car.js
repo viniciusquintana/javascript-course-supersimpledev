8298e6faf355571bb3cdfe64238c60cdfb8768f9
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-04-19 07:24:10 -0300

    finished the exercise 17d

--- Exercises/Lesson-17/data/car.js
+++ Exercises/Lesson-17/data/car.js
@@ -3,6 +3,7 @@ class Car {
   model;
 
   speed = 0;
+  isTrunkOpen = false;
 
   constructor(carDetails) {
     this.brand = carDetails.brand;
@@ -10,13 +11,17 @@ class Car {
   }
 
   displayInfo() {
+    const trunkStatus = this.isTrunkOpen ? 'open' : 'closed';
+
     console.log(
-      `${this.brand} ${this.model}, Speed: ${this.speed}km/h`
+      `${this.brand} ${this.model}, Speed: ${this.speed}km/h, Trunk: ${trunkStatus}`
     );
   }
 
   go() {
-    this.speed += 5;
+    if (!this.isTrunkOpen) {
+      this.speed += 5;
+    }
 
     if (this.speed > 200) {
       this.speed = 200;
@@ -29,6 +34,16 @@ class Car {
     if (this.speed < 0) {
       this.speed = 0;
     }
+  }
+
+  openTrunk() {
+    if (this.speed === 0) {
+      this.isTrunkOpen = true;
+    }
+  }
+
+  closeTrunk() {
+    this.isTrunkOpen = false;
   }
 }
 
@@ -50,6 +65,13 @@ car1.go();
 car1.go();
 car1.brake();
 car1.displayInfo();
+
+car1.openTrunk();
+car1.displayInfo();
+
+car2.openTrunk();
+car2.go();
+car2.displayInfo();
 
 car2.displayInfo();
 car2.go();
